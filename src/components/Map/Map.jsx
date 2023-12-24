336e29b09b14870654e64ab5767cf4da30709d12
Kevin Byrne <kevinspbyrne@gmail.com>
2023-12-23 20:02:48 -0800

    add link to comment form and zindex toggle

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -11,8 +11,13 @@ function Map () {
   const latitude = 49.249814;
   const longitude = -123.1217199;
   const URL = "http://localhost:8080"
-  
   const [posts, setPosts] = useState([])
+  const [mapStyle, setMapStyle] = useState("map")
+
+  function toggleModal () {
+    mapStyle === "map" ? setMapStyle("map map--modal-active") : setMapStyle("map")
+
+  }
 
     async function getPosts () {
       const {data} = await axios.get(`${URL}/posts`)
@@ -32,8 +37,11 @@ function Map () {
   
     return ( 
       <>
-      <section className="map">
-        <MapContainer className="leaf" center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "90vh", width: "100vw"}}>
+      <div className="modal">
+        <button className="map__post" id="test" onClick={toggleModal}>togglemodal</button>
+      </div>
+      <section className={mapStyle}>
+        <MapContainer className="leaf" center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "95vh", width: "100vw"}}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -53,6 +61,10 @@ function Map () {
 
         </MapContainer>
       </section>
+      <div className="map__footer">
+
+      <Link to={'/addcomment'} className="map__post" onClick={toggleModal}>Map Your Mind</Link>
+      </div>
       </>
     );
   };
