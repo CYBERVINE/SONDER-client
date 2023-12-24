e16638839ce0a32b69f18d7a0d1828fbdd7d7aae
Kevin Byrne <kevinspbyrne@gmail.com>
2023-12-23 20:30:07 -0800

    add links and hero to landing page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,25 @@
 import { Link } from "react-router-dom"
+import "./Hero.scss"
 
 function Hero () {
   return (
-    <Link to={'/addcomment'}>Quick Post Anonymously</Link>
+    <>
+    <h1>The World is the post</h1>
+    <h1>And This is the comments section</h1>
+    <h1>Map your inner monolouge</h1>
+    <div className="hero__image"></div>
+    <section className="enterance">
+      <Link to={'/addcomment'} className="enterance__anonymous enterance__card">
+        <p >Quick Post Anonymously</p>
+        <p>Keep it local</p>
+        
+      </Link>
+      <div className="enterance__known enterance__card">
+        <Link to={'/login'}>LOGCIN</Link>
+        <Link to={'/signup'}>SIGN UP</Link>
+      </div>
+    </section>
+    </>
   )
 }
 
