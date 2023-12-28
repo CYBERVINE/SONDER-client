8b1597808924f56d3669450494220fb882555cf9
Kevin Byrne <kevinspbyrne@gmail.com>
2023-12-27 20:07:32 -0800

    style signup / login forms

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import './login.scss'
+
 function Login () {
+  const navigate = useNavigate()
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    navigate('/map')
+  }
   return (
-    <h1>Login</h1>
+    <section className="login">
+    <form className='login__form form' action="submit" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input className='form__input'type="text" name="username" placeholder='Username'/>
+      <input className='form__input' type="password" name="password" placeholder='Confirm Password'/>
+      <button className='form__button' type="submit">Login</button>
+      <div className='link-section'>
+      <p className="link-section__description">Don't have an account? </p><Link className='link-section__link' to={'/signup'}> Sign Up</Link>
+      </div>
+    </form>
+  </section>
   )
 }
 
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,6 +1,30 @@
+import './signup.scss'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
 function Signup () {
+
+  const navigate = useNavigate()
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    navigate('/login')
+  }
   return (
-    <h1>signup</h1>
+    <section className="signup">
+    <form className='form' action="submit" onSubmit={handleSubmit}>
+      <h2>Signup</h2>
+      <input className='form__input'type="text" name="username" placeholder='Username'/>
+      <input className='form__input'type="text" name="city" placeholder='Home City'/>
+      <input className='form__input' type="email" name="email" placeholder='Email'/>
+      <input className='form__input' type="password" name="password" placeholder='Password' />
+      <input className='form__input' type="password" name="password" placeholder='Confirm Password'/>
+      <button className='form__button' type="submit">Sign Up</button>
+      <div className='link-section'>
+      <p className='link-section__description'>Already have an account? </p><Link className='link-section__link' to={'/login'}> Login In</Link>
+      </div>
+    </form>
+  </section>
   )
 }
 
