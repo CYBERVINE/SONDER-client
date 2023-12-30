21000a799e69cd088d5ff4c37dc09f3dba37756f
Kevin Byrne <kevinspbyrne@gmail.com>
2023-12-29 21:02:08 -0800

    add map link hover pointer

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -13,7 +13,7 @@ function EditProfile () {
           <img className='edit__avatar' src="../src/assets/images/smile.jpg" alt="" />
           <div className='edit__profile--text'>
             <label className='edit__label' htmlFor="username">Edit username here:</label>  
-            <input className='edit__input' type="text" name="username" id="username" placeholder='USERNAME'/>
+            <input className='edit__input edit__input--username' type="text" name="username" id="username" placeholder='USERNAME'/>
             <label className='edit__avatar--upload'  htmlFor="avatar">Upload a new avatar
             <input type="file" name="avatar" id="avatar" />
             </label>
@@ -25,7 +25,7 @@ function EditProfile () {
       <form className='edit__form' action="submit">
       <h2 className='edit__heading'>ADD NEW PROMO</h2>
             <label className='edit__label' htmlFor="promo">What's do you want to promote?</label>
-            <textarea className='edit__textarea' type="text" rows="6" />
+            <textarea className='edit__textarea' type="text" rows="10" />
             <label className='edit__label' htmlFor="links">Add the link!</label>
             <input className='edit__input' type="text" />
             <button className='edit__button' type="submit">Post your Promo</button>
