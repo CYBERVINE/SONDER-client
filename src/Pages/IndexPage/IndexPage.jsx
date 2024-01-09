13f843a34ebbdf114bf13b9fe5a7450ea3ef47df
Kevin Byrne <kevinspbyrne@gmail.com>
2024-01-08 17:07:55 -0800

    rewritte landingpage text

--- src/Pages/IndexPage/IndexPage.jsx
+++ src/Pages/IndexPage/IndexPage.jsx
@@ -12,10 +12,10 @@ function IndexPage () {
     </section>
       <div className="index-page__backing">
       </div>
-      <footer className="index-page__footer">
-        <p className="index-page__footer--text index-page__footer--quote"> <b>sonder</b> <i>noun</i> The feeling when you realize that everyone, including strangers passing in the street, has a life as vivid and complex as your own.</p>
-        <p className="index-page__footer--text index-page__footer--quote"> The world inspires thoughts that often go unshared. SONDER is for local creators, comedians, poets, activists, business owners, any one who wants to share thier nuggets of wisdom, and promote their good in the world.</p>
-        <p className="index-page__footer--text index-page__footer--quote"> SONDER is a personal journal, a promotional tool, and an avenue for empathy in your local community.</p>
+      <footer className="index-page__footer">                                                     
+        <p className="index-page__footer--text index-page__footer--quote"> <b>Sonder</b>, <i>noun</i>: The feeling upon realization that all people, including strangers passing by in the street, lives a life as rich and vivid as one's own.</p>
+        <p className="index-page__footer--text index-page__footer--quote"> The world inspires thoughts that often go unshared. SONDER is for local creators, comedians, poets, activists, business owners, and any one who wants to share thier nuggets of wisdom, or promote their good to the world.</p>
+        <p className="index-page__footer--text index-page__footer--quote"> PERSONAL JOURNAL | PROMOTIONAL TOOL | EMPATHY AVENUE</p>
         <div className="index-page__footer--links">  
         <p className="index-page__footer--text">&#169; KEVIN BYRNE </p>
         <a className="index-page__footer--text index-page__footer--link" href="https://linkedin.com/in/kevinbyrne09"> LinkedIn</a>
