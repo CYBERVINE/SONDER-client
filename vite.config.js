f9594a6a52bd030457ce53769b0e394b1de15dff
Kevin Byrne <kevinspbyrne@gmail.com>
2024-01-09 14:52:15 -0800

    add railway build rollupoptions

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 80,
-  // }
+    build: {
+      rollupOptions: {
+        external: ['react-JWT'],
+      },
+    },
 })
