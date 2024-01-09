54e8fe9281ce735d056133777c18570dfe724370
Kevin Byrne <kevinspbyrne@gmail.com>
2024-01-09 14:57:56 -0800

    captialize signup.scss import

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
 import axios from "axios"
-import './login.scss'
+import './Login.scss'
 
 function Login ({getLoginId}) {
   const URL = import.meta.env.VITE_BASE_URL
